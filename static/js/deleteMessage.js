a606fab72340b4efaa02224e4ce440ee8637c124
EduardYan <eduarygp@gmail.com>
2021-12-03 12:22:26 -0500

    some changes in the messages

---
+++ static/js/deleteMessage.js
@@ -0,0 +1,8 @@
+/**
+ * This is a small code for delete the message
+ * showed it.
+ */
+
+const conteiner = document.querySelector('.notes-container');
+const message = document.querySelector('.messages-view');
+conteiner.removeChild(message);
